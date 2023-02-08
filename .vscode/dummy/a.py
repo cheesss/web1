970b98a04be0c74fa02607ca9b160d74c2709bed
cheesss <chohjenderr@gmail.com>
2023-02-08 20:21:14 +0900

    gitignore, 관리자 계정 설정

--- .vscode/dummy/a.py
+++ .vscode/dummy/a.py
@@ -5,5 +5,5 @@ url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=bitcoi
 response = requests.get(url)
 data = response.json()
 
-market_cap = data[0]['market_cap']
+market_cap = data[0]["market_cap"]
 print("Bitcoin market cap: ${:,.2f}".format(market_cap))
--- .vscode/dummy/image.py
+++ .vscode/dummy/image.py
@@ -5,8 +5,7 @@ import time
 
 
 exchange = ccxt.binance()
-symbol = 'BTC/USDT'
-
+symbol = "BTC/USDT"
 
 
 import requests
@@ -20,15 +19,22 @@ if response.status_code == 200:
         datas = response.json()
         transactions_per_second = datas["values"][-1]["y"]
         transactions_per_day = transactions_per_second * 86_400
-        print("Bitcoin on-chain transaction volume: {:,.0f} transactions per day".format(transactions_per_day))
+        print(
+            "Bitcoin on-chain transaction volume: {:,.0f} transactions per day".format(
+                transactions_per_day
+            )
+        )
 
         # convert to dollars
         ticker = exchange.fetch_ticker(symbol)
-        price = ticker['last']
+        price = ticker["last"]
         transaction_volume_usd = transactions_per_day * price
-        print("Bitcoin on-chain transaction volume: ${:,.2f}".format(transaction_volume_usd))
+        print(
+            "Bitcoin on-chain transaction volume: ${:,.2f}".format(
+                transaction_volume_usd
+            )
+        )
     except ValueError:
         print("Could not parse response as JSON.")
 else:
     print("Request failed with status code", response.status_code)
-
