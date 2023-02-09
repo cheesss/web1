e213ea8b0e73c376e7aa56c327ac920879e22519
cheesss <chohjenderr@gmail.com>
2023-02-09 13:58:11 +0900

    cbv 적용

--- blog/urls.py
+++ blog/urls.py
@@ -2,11 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.index),
+    # path('',views.index),
     #url이 blog/로 끝나면 view.py 파일의 index함수를 실행해준다.
     
-    path('<int:pk>/',views.single_post_page),
+    path('', views.PostList.as_view()),
+    #cbv방식 
+
+    # path('<int:pk>/',views.single_post_page),
     #int:pk는 정수 pk값을 전달 받은후, single_post_page에 넘겨준다는 뜻
-    
+
 
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,35 @@
+from django.views.generic import ListView
 from django.shortcuts import render
 from .models import Post
 # Create your views here.
-def index(request):
-    posts = Post.objects.all().order_by('-pk')
 
-    return render(request, 
-    'blog/index.html',
-    {
+# fbv 형식으로 만든 목록 페이지
+# def index(request):
+#     posts = Post.objects.all().order_by('-pk')
 
-        'posts':posts,
-        #데이터베이스와 연결된 인자.
-    }
-    )
+#     return render(request, 
+#     'blog/index.html',
+#     {
 
-def single_post_page(request, pk):
-    post = Post.objects.get(pk=pk)
+#         'posts':posts,
+#         #데이터베이스와 연결된 인자.
+#     }
+#     )
 
-    return render(request,
-        'blog/single_post_page.html',
-        {
-            'post':post,
-        }
-    )
+# def single_post_page(request, pk):
+#     post = Post.objects.get(pk=pk)
+
+#     return render(request,
+#         'blog/single_post_page.html',
+#         {
+#             'post':post,
+#         }
+#     )
+
+
+#cbv형식으로 만들기
+class PostList(ListView):
+    model = Post
+    ordering = '-pk'
+    # template_name = 'blog/index.html'
+    # 템플릿 명을 명시하지 않으면 post_list로 템플릿을 인식한다.
