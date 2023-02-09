c7e0472f2b20c04bff144659d9957db6a1bc88c8
cheesss <chohjenderr@gmail.com>
2023-02-09 12:45:45 +0900

    fbv사용해보기

---
+++ blog/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('',views.index),
+    #url이 blog/로 끝나면 view.py 파일의 index함수를 실행해준다.
+    
+    path('<int:pk>/',views.single_post_page),
+    #int:pk는 정수 pk값을 전달 받은후, single_post_page에 넘겨준다는 뜻
+    
+
+]
